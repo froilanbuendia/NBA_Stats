2ed41fd6953f92692090f0f87c364d0d39bde3fc
froilanbuendia <froilangbuendia@gmail.com>
2025-04-03 02:08:37 -0700

    debugging

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,65 +1,66 @@
 "use client";
-import { NBA_NEWS_API, NBA_TEAMS_API, NBA_TEAM_API } from "./config/config";
-import { useEffect, useState } from "react";
+// import { NBA_NEWS_API, NBA_TEAMS_API, NBA_TEAM_API } from "./config/config";
+// import { useEffect, useState } from "react";
 import Scores from "./scores/Scores";
 
 export default function Home() {
-  const [news, setNews] = useState(null);
-  const [teams, setTeams] = useState(null);
-  const [team, setTeam] = useState(null);
+  // const [news, setNews] = useState(null);
+  // const [teams, setTeams] = useState(null);
+  // const [team, setTeam] = useState(null);
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      if (!NBA_NEWS_API) {
-        console.error("NBA_NEWS_API is undefined");
-        return; // Stop execution if the API URL is missing
-      }
+  // useEffect(() => {
+  //   const fetchNews = async () => {
+  //     if (!NBA_NEWS_API) {
+  //       console.error("NBA_NEWS_API is undefined");
+  //       return; // Stop execution if the API URL is missing
+  //     }
 
-      try {
-        const res = await fetch(NBA_NEWS_API);
-        const result = await res.json();
-        setNews(result);
-        console.log("news:", news, result);
-      } catch (error) {
-        console.error("Error fetching news:", error);
-      }
-    };
+  //     try {
+  //       const res = await fetch(NBA_NEWS_API);
+  //       const result = await res.json();
+  //       setNews(result);
+  //       console.log("news:", news, result);
+  //     } catch (error) {
+  //       console.error("Error fetching news:", error);
+  //     }
+  //   };
 
-    fetchNews();
-  }, []);
-  useEffect(() => {
-    const fetchTeams = async () => {
-      if (!NBA_TEAMS_API) {
-        console.error("NBA_TEAMS_API is undefined");
-        return; // Stop execution if the API URL is missing
-      }
+  //   fetchNews();
+  // }, []);
+  // useEffect(() => {
+  //   const fetchTeams = async () => {
+  //     if (!NBA_TEAMS_API) {
+  //       console.error("NBA_TEAMS_API is undefined");
+  //       return; // Stop execution if the API URL is missing
+  //     }
 
-      try {
-        const res = await fetch(NBA_TEAMS_API);
-        const result = await res.json();
-        setTeams(result);
-        console.log("teams:", teams, result);
-      } catch (error) {
-        console.error("Error fetching teams:", error);
-      }
-    };
+  //     try {
+  //       const res = await fetch(NBA_TEAMS_API);
+  //       const result = await res.json();
+  //       setTeams(result);
+  //       console.log("teams:", teams, result);
+  //     } catch (error) {
+  //       console.error("Error fetching teams:", error);
+  //     }
+  //   };
 
-    fetchTeams();
-  }, []);
+  //   fetchTeams();
+  // }, []);
 
-  useEffect(() => {
-    const fetchTeam = async () => {
-      const res = await fetch(`${NBA_TEAM_API}1`);
-      const result = await res.json();
-      setTeam(result);
-      console.log("team:", team, result);
-    };
+  // useEffect(() => {
+  //   const fetchTeam = async () => {
+  //     const res = await fetch(`${NBA_TEAM_API}1`);
+  //     const result = await res.json();
+  //     setTeam(result);
+  //     console.log("team:", team, result);
+  //   };
 
-    fetchTeam();
-  }, []); // Empty dependency array means it runs once after component mounts
+  //   fetchTeam();
+  // }, []); // Empty dependency array means it runs once after component mounts
 
   return (
     <div>
+      Hello There
       <Scores />
     </div>
   );
